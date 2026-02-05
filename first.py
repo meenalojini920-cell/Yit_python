2aa7769df65692277c0aeed5f491dfdb94a48431
meenalojini920-cell <meenalojini920@gmail.com>
2026-02-05 10:19:29 +0530

    python first

---
+++ first.py
@@ -0,0 +1,8 @@
+name="lorean"
+age=27
+Id=21
+output=f"my name is {name}\nmy name age {age}\nmy id is {Id}"
+print(output)
+
+output="my name is{0}\nmy age {1}\nmy id is {2}".format(name,age,Id)
+print(output)
---
+++ yarlit.py
@@ -0,0 +1,5 @@
+name = "meena"
+age = 23
+Id = 21
+output=f"my name is{name}\nmy age {age}\nmy id is{Id}"
+print(output)
