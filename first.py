cf4051acd14d6ce8cf85a734eba5cb3e6810bbda
meenalojini920-cell <meenalojini920@gmail.com>
2026-02-05 11:00:20 +0530

    Add student info

--- first.py
+++ first.py
@@ -4,5 +4,15 @@ Id=21
 output=f"my name is {name}\nmy name age {age}\nmy id is {Id}"
 print(output)
 
+
 output="my name is{0}\nmy age {1}\nmy id is {2}".format(name,age,Id)
 print(output)
+
+student_Id =30001
+student_name ="lorean"
+group_name ="python"
+student_Nic =200266048
+
+output=f'student id: is {student_Id}\nstudent name {student_name}\ngroup name {group_name}\nstudent NIC no {student_Nic}'
+print(output)
+
